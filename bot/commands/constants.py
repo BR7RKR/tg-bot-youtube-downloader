898cb6394e2b8d1224aff71718ce8889eb3ddffb
Лев <90714959+BR7RKR@users.noreply.github.com>
2023-08-01 14:02:06 +0300

    добавил энам со смайликами для удобства

---
+++ bot/commands/constants.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class Emojis(Enum):
+    EYE = '\U0001F441'
+    MAN = '\U0001F464'
+    CLOCK_1 = '\U0001F570'
+    CAMERA = '\U0001F4FD'
+    INBOX_TRAY = '\U0001F4E5'
+    CALENDAR = '\U0001F4C5'
+    DONE = '\U00002705'
+    IN_PROGRESS = '\U0000231B'
+    CANCELED = '\U0000274C'
+    SONG = '\U0001F3B5'
+    DURATION = '\U0001F551'
+    STONE_FACE = '\U0001F5FF'
