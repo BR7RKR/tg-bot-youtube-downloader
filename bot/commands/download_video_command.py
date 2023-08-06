2cb4224347d708d2e79ae42989adab663b840e7b
Лев <90714959+BR7RKR@users.noreply.github.com>
2023-08-06 12:53:16 +0300

    Реализовал команду для скачивания видео

--- bot/commands/download_video_command.py
+++ bot/commands/download_video_command.py
@@ -1,26 +1,36 @@
+import json
 import os
-import re
+
+import aiofiles
 
 from bot.commands.command import Command
-from clients.tg import Update
+from bot.commands.constants import YOUTUBE_PREFIX
+from clients.tg import Update, CallBackData
 
 
 class DownloadVideoCommand(Command):
     def __init__(self, tg_client, downloader):
         self._tg_client = tg_client
         self._downloader = downloader
+        self._url = YOUTUBE_PREFIX
 
     async def execute(self, upd: Update):
-        title = self._downloader.download_video(url=upd.message.text)
-        video = open(f'video/{title}', 'rb')
+        title = self._downloader.download_video(url=self._url)
+        file = await aiofiles.open(f'video/{title}', 'rb')
         try:
-            await self._tg_client.send_video(upd.message.chat.id, video)
+            video = await file.read()
+            await self._tg_client.send_video(upd.callback_query.message.chat.id, video)
         except Exception as e:
             await self._tg_client.send_message(upd.message.chat.id, "Не получилось отправить видео")
-        video.close()
-        os.remove(f'video/{title}')
+        finally:
+            await file.close()
+            os.remove(f'video/{title}')
 
     def is_for(self, command_definer: Update):
         if command_definer.callback_query is None:
             return False
-        return command_definer.callback_query.data == 'button1'
+        data: str = command_definer.callback_query.data
+        data_json = json.loads(data)
+        data_obj: CallBackData = CallBackData.Schema().load(data_json)
+        self._url += data_obj.data
+        return data_obj.type == 'video'
