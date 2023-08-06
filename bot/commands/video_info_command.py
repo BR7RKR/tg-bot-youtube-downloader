38d003efab35b6b2cd04a557af2a8d4c17c364b9
Лев <90714959+BR7RKR@users.noreply.github.com>
2023-08-06 12:55:22 +0300

    Реализовал команду для отображения информации о видео с кнопками скачивания в двух форматах mp4 и mp3

--- bot/commands/video_info_command.py
+++ bot/commands/video_info_command.py
@@ -1,7 +1,10 @@
+import json
 import re
 
+from pytube import exceptions as pytube_ex
+
 from bot.commands.command import Command
-from bot.commands.constants import Emojis
+from bot.commands.constants import Emojis, YOUTUBE_PREFIX
 from bot.modules.inline_keyboard import InlineKeyboard, InlineKeyboardButton
 from clients.tg import Update, TgClient
 from utils.downloader import YouTubeDownloader
@@ -11,13 +14,21 @@ class VideoInfoCommand(Command):
     def __init__(self, tg_client: TgClient, downloader: YouTubeDownloader):
         self._tg_client = tg_client
         self._downloader = downloader
+        self._url: str = ''
 
     async def execute(self, upd: Update):
-        yt = self._downloader.get_video_info(upd.message.text)
-        photo = yt.thumbnail_url
-        vide_info = self._form_description(yt)
-        reply_markup = self._form_reply_markup()
-        await self._tg_client.send_photo(upd.message.chat.id, photo, vide_info, reply_markup)
+        try:
+            self._url = upd.message.text
+            yt = self._downloader.get_video_info(self._url)
+            photo = yt.thumbnail_url
+            vide_info = self._form_description(yt)
+            reply_markup = self._form_reply_markup()
+            await self._tg_client.send_photo(upd.message.chat.id, photo, vide_info, reply_markup)
+        except pytube_ex.VideoUnavailable as e:
+            await self._tg_client.send_message(upd.message.chat.id, "Не получилось найти видео. Проверьте правильность ссылки.")
+            return
+        except Exception as e:
+            await self._tg_client.send_message(upd.message.chat.id, "Не получилось отправить данные о видео")
 
     def is_for(self, command_definer: Update):
         if command_definer.message is None:
@@ -29,8 +40,12 @@ class VideoInfoCommand(Command):
 
     def _form_reply_markup(self) -> str:
         keyboard = InlineKeyboard()
-        keyboard.add_button(button=InlineKeyboardButton(f"{Emojis.CAMERA.value}mp4", "button1"))
-        keyboard.add_button(button=InlineKeyboardButton(f"{Emojis.SONG.value}mp3", "button2"))
+        prefix = YOUTUBE_PREFIX
+        id = self._url.replace(prefix, "")
+        video_data = {"type": "video", "data": f"{id}"}
+        audio_data = {"type": "audio", "data": f"{id}"}
+        keyboard.add_button(button=InlineKeyboardButton(f"{Emojis.CAMERA.value}mp4", json.dumps(video_data)))
+        keyboard.add_button(button=InlineKeyboardButton(f"{Emojis.SONG.value}mp3", json.dumps(audio_data)))
         return keyboard.reply_markup
 
     def _form_description(self, yt) -> str:
