55d36500656d03388e0b6d3599c459d5dc5f1f1e
Лев <90714959+BR7RKR@users.noreply.github.com>
2023-09-10 15:16:31 +0300

    Добавил ошибку с отрицательным временем

--- bot/engine/exceptions.py
+++ bot/engine/exceptions.py
@@ -2,3 +2,8 @@ class MissingTgClientError(Exception):
     def __init__(self, class_name: str):
         message: str = f"Missing tg client in {class_name}"
         super().__init__(message)
+
+
+class NegativeTimeError(Exception):
+    def __init__(self):
+        message: str = f"Time value is negative"
