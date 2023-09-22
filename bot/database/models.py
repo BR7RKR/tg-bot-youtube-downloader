b298fee9673cb1ef5e8d091ff1870b1513efbd2e
Лев <90714959+BR7RKR@users.noreply.github.com>
2023-09-22 22:00:07 +0300

    обновил модель пользователя

--- bot/database/models.py
+++ bot/database/models.py
@@ -1,7 +1,9 @@
 class User:
 
-    def __init__(self, id=None, full_name=None, role='user', language='en'):
+    def __init__(self, id=None, username=None, firstname=None, lastname=None, role='user', language='en'):
         self.id = id
-        self.full_name = full_name
+        self.username = username
+        self.firstname = firstname
+        self.lastname = lastname
         self.role = role
         self.language = language
