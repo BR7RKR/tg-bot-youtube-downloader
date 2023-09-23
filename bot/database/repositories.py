ba18e30973bdb324ad22b0983880d234f27f58d8
Лев <90714959+BR7RKR@users.noreply.github.com>
2023-09-23 18:28:30 +0300

    Добавил транзакции и немного исправил sql

--- bot/database/repositories.py
+++ bot/database/repositories.py
@@ -23,37 +23,43 @@ class UserRepository: #TODO: прикрутить траназакции
     async def create_table(self):
         conn = await self._get_connection()
 
-        await conn.execute('''
-        CREATE TABLE IF NOT EXISTS users(
-        id TEXT PRIMARY KEY,
-        username TEXT NOT NULL,
-        firstname TEXT NOT NULL,
-        lastname TEXT NOT NULL,
-        role TEXT DEFAULT 'user',
-        language TEXT DEFAULT 'en'
-        )
-        ''')
+        async with conn.transaction():
+            await conn.execute('''
+            CREATE TABLE IF NOT EXISTS users(
+            id TEXT PRIMARY KEY,
+            username TEXT NOT NULL,
+            firstname TEXT NOT NULL,
+            lastname TEXT NOT NULL,
+            role TEXT DEFAULT 'user',
+            language TEXT DEFAULT 'en'
+            )
+            ''')
 
         await conn.close()
 
     async def save_entity(self, entity: User):
         conn = await self._get_connection()
 
-        await conn.execute("INSERT INTO users (id, username, firstname, lastname, role, language) VALUES ($1, $2, $3, $4, $5, $6)",
-                           str(entity.id), entity.username, entity.firstname, entity.lastname, entity.role, entity.language)
+        async with conn.transaction():
+            await conn.execute("INSERT INTO users (id, username, firstname, lastname, role, language) VALUES ($1, $2, $3, $4, $5, $6)",
+                               entity.id, entity.username, entity.firstname, entity.lastname, entity.role, entity.language)
 
         await conn.close()
 
     async def delete_entity(self, entity: User):
         conn = await self._get_connection()
 
-        await conn.execute("DELETE FROM users WHERE id = ?", entity.id)
+        async with conn.transaction():
+            await conn.execute("DELETE FROM users WHERE id = ($1)", entity.id)
 
         await conn.close()
 
     async def get_entity(self, entity_id):
         conn = await self._get_connection()
-        row = await conn.fetchone("SELECT * FROM entities WHERE id = ?", entity_id)
+
+        async with conn.transaction():
+            row = await conn.fetchrow("SELECT * FROM users WHERE id = ($1)", entity_id)
+
         await conn.close()
         if row:
             return User(id=row['id'], username=row['username'], firstname=row['firstname'], lastname=row['lastname'], role=row['role'], language=row['language'])
