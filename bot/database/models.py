23bde7a7361be207bbd3907edd8de6bb5e0137e2
Лев <90714959+BR7RKR@users.noreply.github.com>
2023-09-23 18:19:38 +0300

    написал __eq__ для сравнения двух экземпляров класса User

--- bot/database/models.py
+++ bot/database/models.py
@@ -1,9 +1,21 @@
 class User:
 
-    def __init__(self, id=None, username=None, firstname=None, lastname=None, role='user', language='en'):
-        self.id = id
-        self.username = username
-        self.firstname = firstname
-        self.lastname = lastname
-        self.role = role
-        self.language = language
+    def __init__(self, id, username, firstname, lastname, role='user', language='en'):
+        self.id: str = id
+        self.username: str = username
+        self.firstname: str = firstname
+        self.lastname: str = lastname
+        self.role: str = role
+        self.language: str = language
+
+    def __eq__(self, other):
+        if isinstance(other, User):
+            return (
+                self.id == other.id and
+                self.username == other.username and
+                self.firstname == other.firstname and
+                self.lastname == other.lastname and
+                self.role == other.role and
+                self.language == other.language
+            )
+        return False
